9008936269f12b840b3e10eece13c318c3f10e38
AtharvaMutsaddi <atharvaam21.comp@coep.ac.in>
2022-12-09 10:53:36 +0530

    UPDATE V2

--- boilerplate.c
+++ boilerplate.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #define MAXLEN 500
 int exists(const char *fname)
 {
@@ -12,25 +12,50 @@ int exists(const char *fname)
     }
     return 0;
 }
-void createoropen(const char * argv){
+void createoropen(const char *argv1,const char *argv2)
+{
+    char argv[50];
+    strcat(argv, argv1);
+    strcat(argv, argv2);
+
     if (!exists(argv))
     {
         FILE *ptr = fopen(argv, "w");
         if (ptr == NULL)
         {
-            perror("Soime error occurred: ");
+            perror("Some error occurred: ");
             exit(1);
         }
-        char boiler[MAXLEN] = "#include<stdio.h>\n#include<stdlib.h>\nint main(){\n\n\treturn 0;\n}";
-        fputs(boiler, ptr);
-        fclose(ptr);
-        char command[50]="code ";
-        strcat(command, argv);
-        strcat(command, " .");
-        system(command);
+        if (!strcmp(argv2,".c"))
+        {
+            char boiler[MAXLEN] = "#include<stdio.h>\n#include<stdlib.h>\nint main(){\n\n\treturn 0;\n}";
+            fputs(boiler, ptr);
+            fclose(ptr);
+            char command[50] = "code ";
+            strcat(command, argv);
+            strcat(command, " .");
+            system(command);
+        }
+        else if (!strcmp(argv2,".cpp"))
+        {
+            char boiler[MAXLEN] = "#include<bits/stdc++.h>\nusing namespace std;\nint main(){\n\n\treturn 0;\n}";
+            fputs(boiler, ptr);
+            fclose(ptr);
+            char command[50] = "code ";
+            strcat(command, argv);
+            strcat(command, " .");
+            system(command);
+        }
+        else
+        {
+            perror("The file name u have enetered is not supported by this Utility!");
+        }
+        
+        
     }
-    else{
-        char command[50]="code ";
+    else
+    {
+        char command[50] = "code ";
         strcat(command, argv);
         strcat(command, " .");
         system(command);
@@ -42,7 +67,8 @@ int main(int argc, char const *argv[])
     {
         perror("Please Enter name of prog!: ");
     }
-    createoropen(argv[1]);
     
+    createoropen(argv[1], argv[2]);
+
     return 0;
 }
